587fc37621c6a73adb7f781060af0fd9aa2a0504
Sandro Livino <sandro.livino@gmail.com>
2023-06-29 21:19:11 -0300

    feat: Order Application Service - dto classes

---
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/orderservice/domain/dto/track/TrackOrderQuery.java
@@ -0,0 +1,17 @@
+package com.food.ordering.system.orderservice.domain.dto.track;
+
+import com.food.ordering.system.domain.valueobject.OrderStatus;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.UUID;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class TrackOrderQuery {
+    @NotNull
+    private final UUID orderTrackingId;
+}
