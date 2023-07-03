694f8f8da69dea61e87ef77a9f0472117faae6b4
Sandro Livino <sandro.livino@gmail.com>
2023-07-03 08:38:00 -0300

    fix: order application service implementation - event publisher OPTION 01

--- order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/orderservice/domain/OrderTrackCommandHandler.java
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/orderservice/domain/OrderTrackCommandHandler.java
@@ -9,6 +9,6 @@ import org.springframework.stereotype.Component;
 @Component
 public class OrderTrackCommandHandler {
     public TrackOrderResponse trackOrder(TrackOrderQuery trackOrderQuery){
-
+        return null;
     }
 }
