b0196ccf8853cdf60200fd3571c82800ab70524f
Sandro Livino <sandro.livino@gmail.com>
2023-07-07 14:05:58 -0300

    feat: order data access - added order jpa entities

---
+++ order-service/order-dataaccess/src/main/java/com/food/ordering/system/dataaccess/order/entity/OrderEntityId.java
@@ -0,0 +1,29 @@
+package com.food.ordering.system.dataaccess.order.entity;
+
+import lombok.*;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderEntityId implements Serializable {
+    private Long id;
+    private OrderEntity order;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        OrderEntityId that = (OrderEntityId) o;
+        return id.equals(that.id) && order.equals(that.order);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, order);
+    }
+}
