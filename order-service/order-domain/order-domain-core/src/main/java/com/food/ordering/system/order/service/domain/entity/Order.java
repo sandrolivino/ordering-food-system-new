3d54bab479bc4d3d674385e0f2176be6c41d7a36
Sandro Livino <sandro.livino@gmail.com>
2023-07-10 13:29:09 -0300

    feat: order messaging - listener implementation

--- order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/entity/Order.java
+++ order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/entity/Order.java
@@ -170,6 +170,8 @@ public class Order extends AggregateRoot<OrderId> {
         private OrderStatus orderStatus;
         private List<String> failureMessages;
 
+        public static final String FAILURE_MESSAGE_DELIMITER = ",";
+
         private Builder() {
         }
 
